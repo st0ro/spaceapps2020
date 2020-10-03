3a1bded0d134d582ca3c2a4b37baa3c2180c19f4
benv0 <50381078+benv0@users.noreply.github.com>
2020-10-03 12:05:10 -0600

    updated  nav state

--- src/spaceapps2020/navstate/NavState.java
+++ src/spaceapps2020/navstate/NavState.java
@@ -1,13 +1,12 @@
 package spaceapps2020.navstate;
 
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Image;
-import org.newdawn.slick.SlickException;
+import org.newdawn.slick.*;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 import spaceapps2020.EventBasedState;
+import spaceapps2020.NotTouhou;
 import spaceapps2020.ui.Button;
+import spaceapps2020.ui.ShapeComponent;
 
 public class NavState extends EventBasedState {
     @Override
@@ -17,15 +16,33 @@ public class NavState extends EventBasedState {
 
     @Override
     public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
-        Button backBtn = new Button(gameContainer, 100, 100, 330, 120, new Image("assets/nav/back.png")) {
+        Button backBtn = new Button(gameContainer, 100, 100, 330, 330, new Image("assets/nav/back.png")) {
             @Override
             public void onLeftClick() {
                 stateBasedGame.enterState(0);
             }
         };
         components.add(backBtn);
+
+        Button selectBtn1 = new Button(gameContainer, 700, 1000, 550, 550, new Image("assets/nav/select.png")) {
+            @Override
+            public void onLeftClick() {
+                stateBasedGame.enterState(2);
+            }
+        };
+        components.add(selectBtn1);
+
+        ShapeComponent box = new ShapeComponent(gameContainer, 1650, 530,500,1000, new Color(0x00336),Color.cyan);
+        components.add(box);
+
+        Button debris_one = new Button(gameContainer,1800,400,500,500,new Image("assets/nav/thorabledeb.png")){
+            @Override
+            public void onLeftClick(){System.out.println("dick");}
+        };
+        components.add(debris_one);
+
     }
-    
+
     @Override
     public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) throws SlickException {
 
