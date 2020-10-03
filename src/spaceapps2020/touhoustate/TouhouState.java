f7805e67a4abadcd443a47343dad6bfae41400d2
AntiChange <chr1sf4ng@gmail.com>
2020-10-03 11:49:51 -0600

    we're no strangers

--- src/spaceapps2020/touhoustate/TouhouState.java
+++ src/spaceapps2020/touhoustate/TouhouState.java
@@ -9,6 +9,7 @@ import org.newdawn.slick.state.StateBasedGame;
 public class TouhouState extends BasicGameState {
     public static final boolean DEBUG = true;
     Spaceship ship;
+    Debris debris;
     @Override
     public int getID() {
         return 2;
@@ -17,15 +18,19 @@ public class TouhouState extends BasicGameState {
     @Override
     public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
         ship = new Spaceship();
+        debris = new Debris();
     }
 
     @Override
     public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException {
         ship.render(graphics);
+        debris.render(graphics);
     }
 
     @Override
     public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) throws SlickException {
         ship.update(gameContainer, i);
+        debris.update(gameContainer, i);
+
     }
 }
